7e07a1da40123485eb71d44d462ee061d839d3ae
Gerrit Walther <walther@silpion.de>
2015-01-05 14:37:22 +0100

    Added config files and scripts for i3

---
+++ i3/i3lock.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# The following 2 lines take a screenshot of the current desktop and blur it
+#scrot ~/tmp/deb.png
+#convert ~/tmp/deb.png -blur 2x2 ~/tmp/deb-locked.png
+i3lock -i ~/tmp/deb-locked.png
