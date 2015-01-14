6f555c58652718aad97ed900959199ad48b1f47d
Gerrit Walther <walther@silpion.de>
2015-01-14 12:11:32 +0100

    Fixing the not working autolock

--- i3/i3autolock.sh
+++ i3/i3autolock.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 # got this from: http://rabexc.org/posts/awesome-xautolock-battery
-exec xautolock -detectsleep
-  -time 1 -locker "i3lock -d -i ~/tmp/deb-locked.png" \
-  -notify 30 \
-  -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
+# add -d to i3lock to set screen to standby directly
+exec xautolock -detectsleep -time 1 -locker "i3lock -i ~/tmp/deb-locked.png" -notify 30 -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'"
