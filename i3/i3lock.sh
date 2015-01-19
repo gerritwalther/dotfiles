d7ce5362e2cb34e2656d225d279f1a689df5db6d
Gerrit Walther <walther@silpion.de>
2015-01-19 11:28:38 +0100

    Fixed i3lock to show image on all monitors. Now using i3lock.sh in i3autlock-script.

--- i3/i3lock.sh
+++ i3/i3lock.sh
@@ -3,4 +3,5 @@
 # The following 2 lines take a screenshot of the current desktop and blur it
 #scrot ~/tmp/deb.png
 #convert ~/tmp/deb.png -blur 2x2 ~/tmp/deb-locked.png
-i3lock -i ~/tmp/deb-locked.png
+# -i for a specific image to show, -t to show it on all monitors
+i3lock -i ~/tmp/deb-locked.png -t
