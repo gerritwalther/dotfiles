154863506c88245ef39d575f38a8e654bb74d47b
Gerrit Walther <gw@zenner-iot.com>
2019-04-16 14:46:11 +0200

    Use a random image from ~/Pictures/wp for the lock screen

--- i3/i3lock.sh
+++ i3/i3lock.sh
@@ -3,5 +3,6 @@
 # The following 2 lines take a screenshot of the current desktop and blur it
 #scrot ~/tmp/deb.png
 #convert ~/tmp/deb.png -blur 2x2 ~/tmp/deb-locked.png
+
 # -i for a specific image to show, -t to show it on all monitors
-i3lock -i ~/.i3/deb-locked.png -t
+i3lock -i $(find ~/Pictures/wp/ -type f -name "*.png" | shuf -n 1) -t
